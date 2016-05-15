007c8b5daf8dd8418b8c144dca8ba84e526d7b93
dags <dags-@users.noreply.github.com>
2016-05-15 20:47:23 +0100

    Update Launch.java

--- src/main/java/me/dags/app/Launch.java
+++ src/main/java/me/dags/app/Launch.java
@@ -15,9 +15,7 @@ public class Launch {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }
-        catch (Throwable e)
-        {
+        } catch (Throwable e) {
             e.printStackTrace();
         }
 
